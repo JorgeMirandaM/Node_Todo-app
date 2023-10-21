f709aa1acb0cf113900f200c25bc40e2e88a3190
JorgeMirandaM <jorge100099hotmail.com>
2023-10-21 10:49:39 -0600

    menu created

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 require('colors');
 console.clear()
 
 const main= async()=>{
-    console.log('hola');
+
+    mostrarMenu();
+
+    pausa();
 }
 
 
